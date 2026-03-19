61e58e19afcca5d2e9e141cd5c64ae69b0f5432e
Ameya Joshi <ameyasureshjoshi@gmail.com>
2026-03-19 17:18:17 -0500

    Made the control panel sticky, and made a few more style changes

--- src/components/ControlPanel.tsx
+++ src/components/ControlPanel.tsx
@@ -3,7 +3,8 @@ import '../styles/controlPanel.scss';
 const ControlPanel = () => {
   return (
     <div className='add-new-list'>
-      <i className='fa-solid fa-circle-plus'></i> Create a new list
+      <i className='fa-solid fa-circle-plus'></i>
+      <button className='create-list-button'>Create a new list</button>
     </div>
   );
 };
