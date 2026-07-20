ac3ec5af0520da57563798d67600343ee8e8cb8b
Ameya Joshi <ameyasureshjoshi@gmail.com>
2026-07-20 17:04:26 -0500

    Added the List Actions button the ControlPanel, and also added an animated dropdown menu for it

--- src/components/ControlPanel.tsx
+++ src/components/ControlPanel.tsx
@@ -8,39 +8,57 @@ import { useEffect, useState } from 'react';
 
 const ControlPanel = () => {
   const [isMounted, setIsMounted] = useState(false);
+  const [showListActions, setShowListActions] = useState(false);
 
   const router = useRouter();
   const { editedListIds, saveEditedLists } = useCreateListForm();
   const totalChanges = editedListIds.length;
   const hasUnsavedChanges = editedListIds.length > 0;
 
-  // useEffect(() => {
-  //   console.log('lists:', lists);
-  //   console.log('edited list ids:', editedListIds);
-  //   console.log('has unsaved?', hasUnsavedChanges);
-  // }, [lists, editedListIds, hasUnsavedChanges]);
-
   useEffect(() => {
     setIsMounted(true);
   }, []);
 
   return (
-    <div className='add-new-list'>
+    <div className='control-panel-wrapper'>
       <button
         className='control-panel-create-list-button'
         onClick={() => router.push('/create-list/step-1')}
       >
         <i className='fa-solid fa-circle-plus'></i>
-        Add a new list
       </button>
+
+      <div className='list-actions-button-wrapper'>
+        <button
+          className='control-panel-list-actions-button'
+          onClick={() => setShowListActions((prev) => !prev)}
+        >
+          List Actions
+        </button>
+
+        <div
+          className={`list-actions-button-menu ${
+            showListActions ? 'visible' : ''
+          }`}
+        >
+          <button className='list-actions-button-menu-item'>
+            Edit Current List
+          </button>
+
+          <button className='list-actions-button-menu-item'>Test Button</button>
+        </div>
+      </div>
+
       <button
-        className='save-changes-button'
+        className='control-panel-save-changes-button'
         disabled={isMounted ? !hasUnsavedChanges : false}
         onClick={() => saveEditedLists()}
       >
         Save changes
         <span
-          className={`alert-on-save-changes-button ${hasUnsavedChanges ? 'alert-visible' : ''}`}
+          className={`alert-on-save-changes-button ${
+            hasUnsavedChanges ? 'alert-visible' : ''
+          }`}
         >
           {hasUnsavedChanges && totalChanges}
         </span>
